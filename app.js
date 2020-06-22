0094fd2584b7aabae8483129332866e48cef1623
osidekyle <hawkinskyle67@gmail.com>
2020-06-21 19:01:34 -0700

    successfully made POST request to api

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ function getPosts(){
 function addPost(e){
     e.preventDefault();
 
-    let title = document.getElementById("title").value;
-    let body = document.getElementById("body").value;
+    let title1 = document.getElementById("title").value;
+    let body1 = document.getElementById("body").value;
 
 
     fetch('https://jsonplaceholder.typicode.com/posts',{
@@ -61,8 +61,10 @@ function addPost(e){
             "Accept":"application/json, text/plain, */*",
             'Content-type':'application/json'
         },
-        body:JSON.stringify({title:title,body:body})
-    })
+        body:JSON.stringify({title:title1,body:body1})
+        
+    }
+    )
     .then((res)=> res.json())
     .then((data)=>console.log(data))
 
