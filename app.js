e5fe1fb2f5b7eb695c856a6df5c333f80ddf1bfd
osidekyle <hawkinskyle67@gmail.com>
2020-06-21 18:51:18 -0700

    html and js to post data

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.getElementById("getText").addEventListener('click', getText);
 document.getElementById("getUsers").addEventListener('click', getUsers);
+document.getElementById("getPosts").addEventListener('click', getPosts);
+document.getElementById("addPost").addEventListener('submit', addPost);
 
 
 
@@ -7,7 +9,7 @@ function getText(){
     fetch('sample.txt')
     .then((res)=>res.text())
     .then((data)=>{
-    document.querySelector(".output").innerHTML=data;
+    document.querySelector("#output").innerHTML=data;
     })
     .catch((error)=>console.log(error))
 };
@@ -28,4 +30,40 @@ function getUsers(){
         })
         document.getElementById('output').innerHTML=output;
     })
+}
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res)=>res.json())
+    .then((data)=>{
+        let output = '<h2>Posts</h2>';
+        data.forEach(function(post){
+            output+= `
+            <div>
+            <h3> ${post.title} </h3>
+            <p>${post.body} </p>
+            </div>`
+        })
+        document.getElementById('output').innerHTML=output;
+    })
+}
+
+
+function addPost(e){
+    e.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let body = document.getElementById("body").value;
+
+
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:"POST",
+        headers:{
+            "Accept":"application/json, text/plain, */*",
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({title:title,body:body})
+    })
+    .then((res)=> res.json())
+    .then((data)=>console.log(data))
+
 }
